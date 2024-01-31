e6c07334d7aec7bd076709f18b54abf4c0b9485e
Isaac-Shuman <137553610+Isaac-Shuman@users.noreply.github.com>
2024-01-30 18:59:22 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
-// Filename - App.js
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-import React from "react";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages";
-import Comdishes from "./pages/comdishes";
-import Favdishes from "./pages/favdishes";
-import Signin from "./pages/signin";
-//import Home from "./pages/home";
+import App from "./App";
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/comdishes" element={<Comdishes />} />
-        <Route path="/customize" element={<Favdishes />} />
-        <Route path="/signin" element={<Signin />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
