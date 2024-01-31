69f1ddc692647f2ce2fbf9ffcd8ce3f8b86d97bb
Isaac-Shuman <137553610+Isaac-Shuman@users.noreply.github.com>
2024-01-30 18:46:53 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
+// Filename - App.js
 
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import Comdishes from "./pages/comdishes";
+import Favdishes from "./pages/favdishes";
+import Signin from "./pages/signin";
+//import Home from "./pages/home";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/comdishes" element={<Comdishes />} />
+        <Route path="/customize" element={<Favdishes />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
