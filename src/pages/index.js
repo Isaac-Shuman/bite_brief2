d15cd294656ceb46af475fb9a57a70d4aa72fa9c
Isaac-Shuman <137553610+Isaac-Shuman@users.noreply.github.com>
2024-01-30 18:45:03 -0800

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+// Filename - pages/index.js
+
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>This code was ripped from GeeksforGeeks</h1>
+    </div>
+  );
+};
+
+export default Home;
