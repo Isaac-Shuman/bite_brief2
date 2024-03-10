fdacec061b6e644866401743430bfae826f16a8b
Kelvin Jiang <kelvinkxjiang@gmail.com>
2024-03-09 21:43:06 -0800

    added cookies to store user session

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -3,6 +3,7 @@ import { GoogleLogin } from '@react-oauth/google'
 import { jwtDecode } from 'jwt-decode'
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Cookies from "js-cookie";
 
 export default function SignUp() {
   const [user, setUser] = useState([])
@@ -20,9 +21,11 @@ export default function SignUp() {
       .catch((error) => {
         console.log(error)
       })
-  }
 
-  console.log(user.email)
+    Cookies.set("email", user.email, {
+      expires: 7,
+    });
+  }
 
   return (
     <div>
